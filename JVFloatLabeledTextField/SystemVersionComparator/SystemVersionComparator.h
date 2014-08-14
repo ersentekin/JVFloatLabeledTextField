bd1683819a2dcb610ba9a9453e5d7ac898b4d9e3
ersentekin <ersen.tekin@gmail.com>
2014-08-14 14:26:37 +0300

    Added System Version Comparator

---
+++ JVFloatLabeledTextField/SystemVersionComparator/SystemVersionComparator.h
@@ -0,0 +1,13 @@
+//
+//  SystemVersionComparator.h
+//  Trendyol
+//
+//  Created by Ersen Tekin on 3.04.2014.
+//  Copyright (c) 2014 Trendyol.com. All rights reserved.
+//
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
